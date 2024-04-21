8a2d93ab1ed251f399879bfb34007492362731a8
GigiBeridzeAxal <145029127+GigiBeridzeAxal@users.noreply.github.com>
2024-04-21 11:37:39 +0400

    First Commit

--- app/components/Portfolio.jsx
+++ app/components/Portfolio.jsx
@@ -2,7 +2,33 @@
 import React, { useState } from 'react'
 
 export default function Portfolio() {
+
     const [choosed ,setchoosed] = useState('All')
+    const [myitopened , setmyitopened] = useState(false)
+    const [myitx , setmyitx] = useState()
+    const [myity , setmyity] = useState()
+
+    window.addEventListener('mousemove' , (e) => {
+
+        setmyitx(e.clientX + 50)
+        setmyity(e.clientY)
+    })
+
+    const hiddenmyit = (e) => {
+    
+        setTimeout(() => {
+            setmyitopened(false)
+        }, 1000);
+
+ 
+    }
+   
+    const changepos = (e) => {
+        setmyitx(e.clientX)
+          setmyity(e.clientY) 
+         setmyitopened(true)
+    }
+
     const changechoosed  = (e) => {
 
   
@@ -11,9 +37,13 @@ export default function Portfolio() {
 
     }
   return (
+    
 
     <div className="portfolio">
+        {myitopened == true ? <div className='myitjobscursor bg-gray-300' style={{position:"fixed" , left:myitx , top:myity}} ><img className='portfoliojobsimage' src={'/myitjobs1.png'} width={400} height={400} alt="" /></div> : <div></div>}
+
         <div className="portfolioframe">
+
             <div className="portfoliotittler">Projects</div>
 
             <div className="skillchooser flex pointer align-center justify-center gap-5  " >
@@ -35,6 +65,19 @@ export default function Portfolio() {
 
         
             </div>
+      <br /><br /><br />
+            <div className="portfolioposts " >
+                <a href='https://my-it-jobs-five.vercel.app/' className="mousecover" onPointerLeave={( ) => setmyitopened(false)} onMouseEnter={ (e) => changepos(e)}>
+                <div className="myitjobs   " >
+                <div className="postmaintittle">MYITJOBS</div>
+                <div className="usedtech">Fullstack & Mern Stack Development</div>
+   
+            </div>
+                    
+                </a>
+
+            </div>
+           
         </div>
     </div>
   )
--- app/page.jsx
+++ app/page.jsx
@@ -8,6 +8,7 @@ export default function Home() {
    <>
    <Header></Header>
    <Welcome></Welcome>
+
    <Portfolio></Portfolio>
    </>
   );
